4b00c04a8ab4bca0d662bcd2b9a01493038b5ed3
= <=>
2022-08-10 01:51:42 -0600

    Started quaternions

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,8 @@ extern crate glmath;
 use glmath::*;
 
 fn main() {
-    let a = Vec3f::new(10.0, 5.0, 2.0);
-    let b = Vec3f::new(2.0, 1.0, 3.0);
+    let a = Quatf::new(0.0, 0.0, 0.0, 1.0);
+    dbg!(a);
 
-    println!("{} {}", a, b);
-    dbg!(a % b);
-    dbg!(b % a);
+    dbg!(a.length());
 }
